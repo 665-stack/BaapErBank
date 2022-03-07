5b60b6091b3ddc2abb777576ad96ae4580deb7a4
unknown <mahmodulshahin665@gmail.com>
2022-03-07 20:27:04 +0600

    add form

---
+++ JS/bank.js
@@ -0,0 +1,13 @@
+document.getElementById('login-submit').addEventListener('click', function () {
+    //get user email
+    const emailField = document.getElementById('user-email');
+    const userEmail = emailField.value;
+    //get user password
+    const passwordField = document.getElementById('user-password');
+    const userPassword = passwordField.value;
+
+    //submit btn handler; akhne jodi shorto ta true hoy taile amake arekta page e niye jabe;
+    if (userEmail == 'devil@mail.com' && userPassword == 'psd') {
+        window.location.href = 'banking.html';
+    }
+})
